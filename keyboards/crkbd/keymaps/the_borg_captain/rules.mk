d07e53e510bad90693c76b9a3ffceef8cadc497c
The-Borg-Captain <20614248+The-Borg-Captain@users.noreply.github.com>
2023-03-01 17:47:53 -0600

    add my chocifi keymap

---
+++ keyboards/crkbd/keymaps/the_borg_captain/rules.mk
@@ -0,0 +1,11 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+WS2812_DRIVER = vendor
+
+# Bootloader selection
+BOOTLOADER = rp2040
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
